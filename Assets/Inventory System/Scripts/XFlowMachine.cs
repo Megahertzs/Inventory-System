399ad0da5cbccf9b4b4663fdf79b0f1773fd52d4
Megahertzs <68621933+Megahertzs@users.noreply.github.com>
2020-11-23 01:02:46 -0600

    First Push

---
+++ Assets/Inventory System/Scripts/XFlowMachine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ludiq;
+using Bolt;
+
+[IncludeInSettings(true)]
+public static class XFlowMachine
+{ 
+    public static void InstantiateMachine(GameObject target, FlowMacro macro)
+    {
+        target.AddComponent<FlowMachine>().nest.SwitchToMacro(macro);
+    }
+
+}
